c3c5dac4f05d2f4f2b2f86d3b478fbe13860aaef
drchain <phvietan@gmail.com>
2022-03-23 19:28:58 +0800

    add

---
+++ hacks/ipCheck.js
@@ -0,0 +1,37 @@
+const {PacketQL, FuzzStatus} = require('@drstrain/shadeless-lib');
+const fs = require('fs/promises');
+const {exec,getRandomString} = require('@drstrain/drutil');
+const creds = require('./creds.json');
+
+function getHostFromHeaders(headers) {
+  for (let i = 0; i < headers.length; ++i) {
+    const [key, val] = headers[i].split(': ');
+    if (key.toLowerCase() === 'host') return val;
+  }
+}
+
+async function main() {
+  const pql = new PacketQL({
+    choosingProject: 'test',
+    ...creds,
+  });
+  const packets = await pql
+      .setStatus(FuzzStatus.ANY)
+      .setRequestHeader('host:')
+      .query();
+
+  const hosts = packets.map(({ requestHeaders }) => getHostFromHeaders(requestHeaders));
+  const uniqeHosts = [...new Set(hosts)];
+
+  const randomName = getRandomString(8) + '.txt';
+  await fs.writeFile(randomName, uniqeHosts.reduce((prev, now) => prev+now+'\n'), '');
+
+  console.log(`Found ${uniqeHosts.length} hosts, running ip check`);
+  const {stdout} = await exec('python3', ['hosts.py', randomName]);
+  console.log(stdout);
+
+  await fs.rm(randomName);
+  process.exit(0);
+}
+
+main().then().catch((err) => console.log(err));
